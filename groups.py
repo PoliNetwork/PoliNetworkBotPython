00043689155b62d917b36b7586931395d569244d
Federico Armellini <armellini.federico@gmail.com>
2019-07-28 15:26:07 +0200

    prima bozza del file groups.py

--- groups.py
+++ groups.py
@@ -1,2 +1,8 @@
 def try_add_group(message):
-    print(message)
+    chat = message['chat']
+    if chat['type'] == 'private':
+        print('Received a private message.')
+        return
+
+    #todo: add group to json
+    print(chat)
