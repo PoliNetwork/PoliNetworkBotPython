3fa8aa0fdca0af84d256185ca02ee85401350ffb
Federico Armellini <armellini.federico@gmail.com>
2019-07-29 12:14:02 +0200

    escape html name

--- functions/utils.py
+++ functions/utils.py
@@ -3,6 +3,13 @@ import datetime
 import bot
 
 
+def escape(html):
+    """Returns the given HTML with ampersands, quotes and carets encoded."""
+    html = str(html)
+    return (html.encode('utf-8')).replace('&', '&amp;').replace('<', '&lt;') \
+        .replace('>', '&gt;').replace('"', '&quot;').replace("'", '&#39;')
+
+
 def send_in_private_or_in_group(text, group_id, user):
     success = True
     user_id = user.id
@@ -15,9 +22,19 @@ def send_in_private_or_in_group(text, group_id, user):
     if success is True:
         return
 
+    message_to = "";
     if user.username is not None and user.username != "":
-        text = "[Messaggio per @" + user.username + "]\n" + text
+        message_to = "@" + user.username
+    else:
+        nome = user['first_name']
+        if user['last_name'] is not None and user['last_name'] != "":
+            nome = nome + " " + user["last_name"]
 
-    done2 = bot.updater.bot.send_message(group_id, text)
+        nome = str(escape(nome))
+        message_to = "<a href='tg://user?id=" + user_id + "'>" + nome + "</a>"
+
+    text = "[Messaggio per " + message_to + "]\n" + text
+
+    done2 = bot.updater.bot.send_message(group_id, text, parse_mode="HTML")
 
     # to_delete.add(group_id, done2.message_id, datetime.datetime.now())
