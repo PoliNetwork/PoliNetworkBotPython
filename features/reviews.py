d25e7d872b15c0158e839f5b1abd28c689628b14
zandre00 <andreazito1@hotmail.com>
2019-07-29 10:16:23 +0200

    Added the author's id check to avoid multiple votes

--- features/reviews.py
+++ features/reviews.py
@@ -19,30 +19,59 @@ def add_review(update, context):
         return
 
     salt = open("salt.txt", encoding="utf-8").read()
-
     text = message.text
+
+    # Review's attributes + hash
+
     vote = text.split(" ")[1]
     description = " ".join(text.split(" ")[2:])
     group_id = str(chat['id'])
-
     to_hash = str(chat['id']).join(salt).encode('utf-8')
     hash2 = hashlib.sha512(to_hash).hexdigest()
     author_id = str(hash2)
+
+    '''
+    So, the reviews_dict is made in the following way:
+    "group_id : [{
+        author: ""
+        vote: ""
+        description: ""
+     }], [...], [...]
+     Let's assume group_id as the key of the dict. While the jsons are the reviews that are in that group.
+     Pretty easy, isnt it?!
+    '''
 
     group_reviews = []
     if reviews_dict and reviews_dict.keys().__contains__(group_id):
         group_reviews = reviews_dict.get(group_id)
 
-    # Create the json
+    # Lets check if the author has already voted that group
+
+    author_already_voted = False
+    for group in group_reviews:
+        if group['author_id'] == author_id:
+            author_already_voted = True
+    if author_already_voted:
+        bot.updater.bot.deleteMessage(chat_id=update.message.chat_id,
+                                  message_id=update.message.message_id)
+        return
+
+    # Create the json that will be appended to the list
+
     save = {
         "author_id": author_id,
         "vote": vote,
         "description": description
     }
-    # Add to existing list of reviews the json built
+
+    # Add the json built to the existing list of reviews
+
     group_reviews.append(save)
-    # Create a dict made up by group id and reviews linked to that group
+
+    # Replace the original group_reviews with the new one and save everything on the file
+
     reviews_dict.update({group_id: group_reviews})
+
     # Save everything and delete the message sent by user
     with open("data/reviews.json", 'w', encoding="utf-8") as file_to_write:
         json.dump(reviews_dict, file_to_write)
