839f9e3297ba727e4c5b2a5a5620a306af2470d2
Federico Armellini <armellini.federico@gmail.com>
2019-07-29 10:47:14 +0200

    new utils function: try to send in private, or else send in group

--- functions/utils.py
+++ functions/utils.py
@@ -1,2 +1,20 @@
+import bot
+
+
 def send_in_private_or_in_group(text, group_id, user_id):
-    pass
+    success = True
+
+    try:
+        done = bot.updater.bot.send_message(user_id, text)
+    except:
+        success = False
+
+    try:
+        get_id = done.chat.id
+        if get_id is None or get_id == "":
+            success = False
+    except:
+        success = False
+
+    if success is False:
+        bot.updater.bot.send_message(group_id, text)
--- main.py
+++ main.py
@@ -40,7 +40,12 @@ dispatcher.add_handler(CommandHandler('getreviewjson', reviews.get_review_json))
 
 def test_message(update, context):
     message = update.message
-    utils.send_in_private_or_in_group("Ciao!", message.chat.id, message['from'].id)
+
+    if message.chat.type == "private":
+        return
+
+    from_user = message['from_user']
+    utils.send_in_private_or_in_group("Ciao!", message.chat.id, from_user.id)
 
 
 dispatcher.add_handler(CommandHandler('testmessage', test_message))
