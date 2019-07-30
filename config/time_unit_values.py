cb1a75a9da668f7d3d07de75cd55227441ec129b
zandre00 <andreazito1@hotmail.com>
2019-07-30 23:21:06 +0200

    Added the ban/mute command (TESTED) Added ban all command (NOT TESTED YET)

--- config/time_unit_values.py
+++ config/time_unit_values.py
@@ -3,15 +3,17 @@ hours = ["hours", "h", "ore"]
 minutes = ["minutes", "m", "min", "minuti"]
 seconds = ["seconds", "s", "sec", "secondi"]
 
+# e.g. s = 10 s
+
 
 def convert_time_in_seconds(s):
-    time = s[0]
-    unit = s[1]
+    time = float(s.split(" ")[0])
+    unit = s.split(" ")[1]
     if days.__contains__(unit):
-        return float(time) * 24 * 60 * 60;
+        return time * 24 * 60 * 60;
     if hours.__contains__(unit):
-        return float(time) * 60 * 60
+        return time * 60 * 60
     if minutes.__contains__(unit):
-        return float(time) * 60
+        return time * 60
     else:
         return time
--- features/moderation.py
+++ features/moderation.py
@@ -2,39 +2,43 @@ import datetime
 
 import variable
 from config import creators, time_unit_values
+from functions import utils
 
 
-def mute(update, context):
+def mute_ban(update, context):
     bot = variable.updater.bot
     message = update.message
+    command = message['text'].split(" ")[0].replace("/","")
     admins = bot.get_chat_administrators(message['chat']['id'])
 
     chat_id = message.chat_id
 
     admins = {admin["user"]['id']
-                        for admin in admins}
+              for admin in admins}
 
     sender = message.from_user['id']
 
-    if message.reply_to_message is None or message.reply_to_message is "":
+    if not (sender in admins):
+        utils.send_in_private_or_in_group("Comando vietato. Non sei admin.", chat_id, message.from_user)
         return
+
+
+    if message.reply_to_message == None:
+        utils.send_in_private_or_in_group("Devi rispondere al messaggio dell'utente che vuoi bannare per eseguire tale azione.", chat_id, message.from_user)
+        return
+
+    time = None
 
     receiver = message.reply_to_message.from_user['id']
 
-    if not (sender in admins):
-        return
-
     if len(message.text.split(" ")) > 1:
-        time = time_unit_values.convert_time_in_seconds(message.text.split(" ")[1:])
-        bot.restrict_chat_member(chat_id, receiver, until_date=datetime.datetime.now().timestamp() + time)
+        time = float(datetime.datetime.now().timestamp()) + \
+               time_unit_values.convert_time_in_seconds(" ".join(message.text.split(" ")[1:]))
+
+    if command in "mute":
+        bot.restrict_chat_member(chat_id, receiver, until_date = time)
     else:
-        bot.restrict_chat_member(chat_id, receiver)
-
-
-def ban(update, context):
-    # todo: ban user of replied message
-    # todo: check if the one that is using the command is admin
-    return None
+        bot.kick_chat_member(chat_id, receiver, until_date = time)
 
 
 def ban_all(update, context):
@@ -43,4 +47,13 @@ def ban_all(update, context):
     if chat.id not in creators.owners:  # only owners can do this command
         return
 
-    # todo: ban user from all groups.
+    if message.reply_to_message == None:
+        utils.send_in_private_or_in_group("Devi rispondere al messaggio dell'utente che vuoi bannare per eseguire tale azione.", chat_id, message.from_user)
+        return
+
+    receiver = message.reply_to_message.from_user['id']
+
+    for group in variable.groups_list:
+        variable.updater.bot.restrict_chat_member(chat.id, receiver)
+
+
