cb60ad51a5b0ae30713c7ae25b037e46fcf14906
Federico Armellini <armellini.federico@gmail.com>
2019-07-30 17:22:11 +0200

    more time unit

--- config/time_unit_values.py
+++ config/time_unit_values.py
@@ -1,16 +1,17 @@
 days = ["giorni", "days", "d"]
-hours = ["hours","h"]
-minutes = ["minutes","m"]
-seconds = ["seconds","s"]
+hours = ["hours", "h", "ore"]
+minutes = ["minutes", "m", "min", "minuti"]
+seconds = ["seconds", "s", "sec", "secondi"]
 
 
 def convert_time_in_seconds(s):
     time = s[0]
     unit = s[1]
     if days.__contains__(unit):
-        return float(time)*24*60*60;
+        return float(time) * 24 * 60 * 60;
     if hours.__contains__(unit):
-        return float(time)*60*60
+        return float(time) * 60 * 60
     if minutes.__contains__(unit):
         return float(time) * 60
-    else: return time
+    else:
+        return time
