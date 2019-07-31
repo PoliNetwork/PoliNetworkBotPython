46e9a094c05d74f28de1acd72ba8b22f2a2e066f
Federico Armellini <armellini.federico@gmail.com>
2019-07-31 09:42:11 +0200

    check all messages

--- features/all_messages.py
+++ features/all_messages.py
@@ -1,6 +1,23 @@
 from features import groups
+from functions import utils
+
+
+def check_blacklist(message):
+    chat = message.chat
+
+    if chat.type == "private":
+        return
+    text = message.text
+
+    is_valid = utils.is_valid(text)
+
+    if is_valid is False:
+        # todo: mute for 15 minutes and delete message.
+        pass
 
 
 def check_message(update, context):
-    # update.message.reply_text("Message received")
-    groups.try_add_group(update.message)
+    message = update.message
+    groups.try_add_group(message)
+
+    check_blacklist(message)
