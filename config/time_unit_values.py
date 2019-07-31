c79ba980849879943294e44686f502ff67a22ec1
Federico Armellini <armellini.federico@gmail.com>
2019-07-31 08:27:47 +0200

    years and months

--- config/time_unit_values.py
+++ config/time_unit_values.py
@@ -2,13 +2,19 @@ days = ["giorni", "days", "d"]
 hours = ["hours", "h", "ore"]
 minutes = ["minutes", "m", "min", "minuti"]
 seconds = ["seconds", "s", "sec", "secondi"]
+months = ["mon", "mesi", "months"]
+years = ["y", "years", "anni"]
 
 # e.g. s = 10 s
 
 
 def convert_time_in_seconds(s):
+    if years.__contains__(s):
+        return 24 * 60 * 60 * 365
+    if months.__contains__(s):
+        return 24 * 60 * 60 * 30
     if days.__contains__(s):
-        return 24 * 60 * 60;
+        return 24 * 60 * 60
     if hours.__contains__(s):
         return 60 * 60
     if minutes.__contains__(s):
