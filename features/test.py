e88237ec12829bddf22d9034f0eeb40f2e1eed7e
Federico Armellini <armellini.federico@gmail.com>
2019-08-01 11:46:18 +0200

    stress test

--- features/test.py
+++ features/test.py
@@ -1,3 +1,4 @@
+import variable
 from functions import utils
 
 
@@ -9,3 +10,18 @@ def test_message(update, context):
 
     from_user = message['from_user']
     utils.send_in_private_or_in_group("Ciao!", message.chat.id, from_user)
+
+
+def stress_test(update, context):
+    # todo: place a return on first line so this method is disabled.
+
+    message = update.message
+    from_user = message.from_user
+
+    i = 0
+    n = 10000
+    while i <= n:
+        if i == n:
+            variable.updater.bot.send_message(from_user.id, "Completate " + str(n) + " iterazioni!")
+        i = i + 1
+    return None
--- main.py
+++ main.py
@@ -40,6 +40,7 @@ dispatcher.add_handler(CommandHandler('ottieni_recensioni', reviews.get_reviews_
 dispatcher.add_handler(CommandHandler('getgroupjson', groups.get_group_json))
 dispatcher.add_handler(CommandHandler('getreviewjson', reviews.get_review_json))
 dispatcher.add_handler(CommandHandler('testmessage', test.test_message))
+dispatcher.add_handler(CommandHandler('stress_test', test.stress_test))
 
 # moderation
 dispatcher.add_handler(CommandHandler('mute', moderation.mutes_bans_handler))
