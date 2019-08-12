106d8055997133ae73701bb93b144d92def90d4c
Federico Armellini <armellini.federico@gmail.com>
2019-08-12 17:26:09 +0200

    restart (written in c++)

---
+++ restart.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+string command(string t, bool has_output = true)
+{
+	cout<<endl<<endl;
+	cout<<"I'm about to do this command:"<<endl;
+	cout<<t<<endl;
+	
+	string r = "";
+	FILE *fp;
+	char path[1035];
+	
+	/* Open the command for reading. */
+	fp = popen(t.c_str(), "r");
+	if (fp == NULL) {
+		printf("Failed to run command\n" );
+		return NULL;
+	}
+	
+	if (has_output)
+	{
+		/* Read the output a line at a time - output it. */
+		while (fgets(path, sizeof(path)-1, fp) != NULL) {
+			r += path;
+		}
+	}
+	
+	/* close */
+	pclose(fp);
+	
+	return r;
+}
+
+int main( int argc, char *argv[] )
+{
+	string output;
+	
+	command("cd /home/ec2-user/pnb");
+	command("git pull origin master");
+	output = command("ps -ax | grep python3 | grep -Eo '^[^ ]+'");
+	
+	stringstream test(output.c_str());
+	string segment;
+	
+	while(getline(test, segment, '\n'))
+	{
+		command("kill " + segment);
+	}
+	
+	command("python3 main.py &", false);
+
+	return 0;
+}
