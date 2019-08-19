e310e0502e0bcc5d3fef5226705b9e863ffec36d
Federico Armellini <armellini.federico@gmail.com>
2019-08-19 15:48:12 +0200

    config updated

--- config/creators.py
+++ config/creators.py
@@ -1,4 +1,4 @@
-creators = ["PoliCreator", "PoliCreator2"]
+creators = ["PoliCreator", "PoliCreator2", "PoliCreator3"]
 owners = [5651789]  # id of @ArmeF97
 me = -1
 allowed_spam = [5651789  # @ArmeF97
