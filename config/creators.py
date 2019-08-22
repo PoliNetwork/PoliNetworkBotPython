e8687074fe5ff924ce2fd0bba8ddcfb6b75c54ef
Federico Armellini <armellini.federico@gmail.com>
2019-08-22 21:54:33 +0200

    policreator4

--- config/creators.py
+++ config/creators.py
@@ -1,4 +1,4 @@
-creators = ["policreator", "policreator2", "policreator3"]
+creators = ["policreator", "policreator2", "policreator3", "policreator4"]
 owners = [5651789]  # id of @ArmeF97
 me = -1
 allowed_spam = [5651789  # @ArmeF97
