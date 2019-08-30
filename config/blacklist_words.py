7cf55d453bd4f09a3301accb40cd0738388ff369
Federico Armellini <armellini.federico@gmail.com>
2019-08-30 08:00:36 +0200

    config black domain list

--- config/blacklist_words.py
+++ config/blacklist_words.py
@@ -1,5 +1,6 @@
 blacklist_words = ["porcodio", "dioporco", "diocane"]
-blacklist_sites = ["facebook.com", "t.me", "whatsapp.com"]  # sites with potential spam
+blacklist_sites = ["facebook.com", "t.me", "whatsapp.com", "instagram.com"]  # sites with potential spam
 allowed_tags = ["@poligruppo", "@infopolimi", "@polirules",
                 "@polinetwork", "@polibook", "@polimispotted",
-                "@polimimemes"]  # remember to save in lower() string
+                "@polimimemes", "@poliextra", "@polileonardo",
+                "@polibovisa"]  # remember to save in lower() string
