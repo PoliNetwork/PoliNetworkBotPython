b9ac607122238b36f8d7d31433f91204608a4b38
Federico Armellini <armellini.federico@gmail.com>
2019-09-02 22:57:39 +0200

    todo anon

--- features/anonimi.py
+++ features/anonimi.py
@@ -1,2 +1,17 @@
 def post_anonimi(update, context):
+    # todo: if chat is private go on, else return try write him (in group or in private) the error
+
+    # todo if message is replying to another message go on, else return and write him the error
+
+    # todo send message to group for approval
+    # todo send a second message, replying the first one, with a keyboard for accepting it
+    # todo send a message to the user "Il messaggio è stato inoltrato e in attesa di approvazione"
+
+    # todo: (not sure if here in this part of code)
+    #   if accept: post to channel and send the link to the original user "Il tuo messaggio è stato pubblicato, qui il link: [link]"
+    #   if not accept: send to the original user
+    #                   "Il tuo messaggio è stato rifiutato.
+    #                   Controlla di aver rispettato le regole del network @PoliRules,
+    #                   e nel caso credi sia stato un errore,
+    #                   scrivici nella pagina facebook di PoliNetwork, grazie"
     return None
