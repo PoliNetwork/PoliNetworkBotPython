0a60aa11494f4825dcf281098d22556f033d25c9
Federico Armellini <armellini.federico@gmail.com>
2019-09-03 11:40:17 +0200

    recordings

--- features/recordings.py
+++ features/recordings.py
@@ -1,6 +1,13 @@
 from functions import utils
 
 
+def post_recordings2(update):
+    message = update.message
+    # todo: forward audio or voice to @PoliRecordings
+    utils.send_in_private_or_in_group("Audio correttamente inoltrato in @PoliRecordings",
+                                      message.chat.id, message.from_user)
+
+
 def post_recordings(update, context):
     message = update.message
 
@@ -15,4 +22,10 @@ def post_recordings(update, context):
                                           message.chat.id, message.from_user)
         return
 
-    pass
+    if message.reply_to_message.voice is not None or message.reply_to_message.audio is not None:
+        post_recordings2(update)
+        return
+    else:
+        utils.send_in_private_or_in_group("Questo comando funziona solo se rispondi ad un messaggio (di tipo audio), "
+                                          "il quale messaggio sarà poi inviato in @PoliRecordings",
+                                          message.chat.id, message.from_user)
