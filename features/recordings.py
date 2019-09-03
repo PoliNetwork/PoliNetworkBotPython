29b32aa76f2f38d55ad50e13587c153baf10dd5d
Federico Armellini <armellini.federico@gmail.com>
2019-09-03 11:31:49 +0200

    recordings

--- features/recordings.py
+++ features/recordings.py
@@ -1,2 +1,16 @@
+from functions import utils
+
+
 def post_recordings(update, context):
-    pass
+    message = update.message
+
+    if message.chat.type != "private":
+        utils.send_in_private_or_in_group("Questo comando funziona solo in chat privata",
+                                          message.chat.id, message.from_user)
+        return
+
+    if message.reply_to_message is None:
+        utils.send_in_private_or_in_group("Questo comando funziona solo se rispondi ad un messaggio (di tipo audio), "
+                                          "il quale messaggio sarà poi inviato in @PoliRecordings",
+                                          message.chat.id, message.from_user)
+        return
