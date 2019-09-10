9948cb62798dda880e443cabe932f61325a3aa15
Federico Armellini <armellini.federico@gmail.com>
2019-09-10 14:28:08 +0200

    blacklist

--- config/blacklist_words.py
+++ config/blacklist_words.py
@@ -1,5 +1,6 @@
 blacklist_words = ["porcodio", "dioporco", "diocane"]
-blacklist_sites = ["facebook.com", "t.me", "whatsapp.com", "instagram.com"]  # sites with potential spam
+blacklist_sites = ["facebook.com", "t.me", "whatsapp.com",
+                   "instagram.com", "bit.ly", "is.gd"]  # sites with potential spam
 allowed_tags = ["@poligruppo", "@infopolimi", "@polirules",
                 "@polinetwork", "@polibook", "@polimispotted",
                 "@polimimemes", "@poliextra", "@polileonardo",
