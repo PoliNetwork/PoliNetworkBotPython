69e1e34f0d59e5ae7a4bf165ee9943f1b14f3735
Federico Armellini <armellini.federico@gmail.com>
2019-09-20 22:55:07 +0200

    more site banned (spam)

--- config/blacklist_words.py
+++ config/blacklist_words.py
@@ -1,6 +1,10 @@
+
 blacklist_words = ["porcodio", "dioporco", "diocane"]
+
 blacklist_sites = ["facebook.com", "t.me", "whatsapp.com",
-                   "instagram.com", "bit.ly", "is.gd"]  # sites with potential spam
+                   "instagram.com", "bit.ly", "is.gd",
+                   "amzn.to", "goo.gl"]  # sites with potential spam
+
 allowed_tags = ["@poligruppo", "@infopolimi", "@polirules",
                 "@polinetwork", "@polibook", "@polimispotted",
                 "@polimimemes", "@poliextra", "@polileonardo",
