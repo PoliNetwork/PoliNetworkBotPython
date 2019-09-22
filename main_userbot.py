57149e43c04ff2ad4cea4644bd924d8ca17e1fa4
Federico Armellini <armellini.federico@gmail.com>
2019-09-22 17:04:12 +0200

    userbot, pt.3

--- main_userbot.py
+++ main_userbot.py
@@ -1,8 +1,22 @@
 import pyrogram
 from pyrogram import Client
 
-from config import userbot_config
+from config import userbot_config, creators
 from functions import utils
+
+
+def create_group(app, group_name):
+    # todo: create a group and set it correctly.
+    pass
+
+
+def message_from_owner(app, message):
+    text = str(message.text)
+    if text.startswith("/create"):
+        group_name = text[7:]
+        create_group(app, group_name)
+    else:
+        message.reply_text("Comando non valido.".format(message.from_user.first_name))
 
 
 def main():
@@ -15,6 +29,14 @@ def main():
                 app.read_history(message.chat.id)
             except Exception as e:
                 utils.notify_owners(e, "userbot non riesce a leggere i messaggi")
+
+            try:
+                if message.chat.id in creators.owners:
+                    message_from_owner(app, message)
+                    return
+            except Exception as e:
+                utils.notify_owners(e, "userbot non riesce a controllare il messaggio")
+
             message.reply_text("Hello {}".format(message.from_user.first_name))
 
         app.run()
