ab18fd2719544a7dc587b8891ad60dd139bfaeab
Federico Armellini <armellini.federico@gmail.com>
2019-10-25 17:17:44 +0200

    .

--- features/associazioni.py
+++ features/associazioni.py
@@ -173,13 +173,14 @@ def send_scheduled_messages():
     db_associazioni.config_json.update({"date": db_associazioni.date})
     save_date()
 
+
     for associazione in db_associazioni.messages_dict:
         try:
             associazione2 = db_associazioni.messages_dict.get(associazione)
             associazione2['chat_id'] = 0
             associazione2['message_id'] = 0
             associazione2['time'] = None
-            
+
             db_associazioni.messages_dict[associazione] = associazione2
 
         except Exception as e:
