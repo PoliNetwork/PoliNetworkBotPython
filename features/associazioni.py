e6be2be31e50fc8fe865cc9fc00133301f984a57
Federico Armellini <armellini.federico@gmail.com>
2019-12-06 22:51:01 +0100

    message from object, pt.4

--- features/associazioni.py
+++ features/associazioni.py
@@ -307,9 +307,19 @@ def assoc_write(update, context):
 
 
 def getUserFromObject(message_from_user):
+
+    if message_from_user is None:
+        return None
+
+    lc = None
+    try:
+        lc = message_from_user.language_code
+    except:
+        pass
+
     r = {"id": message_from_user.id, "first_name": message_from_user.first_name,
          "last_name": message_from_user.last_name, "username": message_from_user.username,
-         "language_code": message_from_user.language_code}
+         "language_code": lc}
     return r
 
 
@@ -465,6 +475,12 @@ def messageFromObject(message_object):
     if message_object is None:
         return None
 
+    html = None
+    try:
+        html = str(message_object.caption_html)
+    except:
+        pass
+
     r = {
         "message_id": message_object.message_id,
         "reply_to_message": messageFromObject(message_object.reply_to_message),
@@ -472,8 +488,10 @@ def messageFromObject(message_object):
         "audio": getAudioFromObject(message_object.audio),
         "video": getVideoFromObject(message_object.video),
         "voice": getVoiceFromObject(message_object.voice),
-        "caption_html": message_object.caption_html,
-        "text": message_object.text
+        "caption_html": html,
+        "text": message_object.text,
+        "date": str(message_object.date),
+        "from_user": getUserFromObject(message_object.from_user)
     }
 
     return r
@@ -507,7 +525,6 @@ def send_scheduled_messages2():
                     if contains_dict(associazione, "sent") is False:
                         db_associazioni.messages_dict[associazione]['sent'] = []
 
-                    # todo: cambiare "forse inviato" con delle info migliori sul messaggio, data e link
                     db_associazioni.messages_dict[associazione]['sent'].append(messageFromObject(forse_inviato))
 
                 else:
