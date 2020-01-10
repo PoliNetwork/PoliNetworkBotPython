c61ad6cfa32a5425c773ac0ba0ddd8c84c9ee5e3
Federico Armellini <armellini.federico@gmail.com>
2020-01-10 17:40:58 +0100

    .

--- sub_bots/anon/variable_anon.py
+++ sub_bots/anon/variable_anon.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater
 
-#token = open("sub_bots/anon/token_anon.txt").read()
-token = open("token.txt").read()
+token = open("sub_bots/anon/token_anon.txt").read()
+# token = open("token.txt").read()
 updater = Updater(token, use_context=True)
 
 
