b81b1a63fe6e463925ef9d2d2f13b111519d6cfe
Federico Armellini <armellini.federico@gmail.com>
2020-01-13 10:17:07 +0100

    anon, pt.4

--- functions/temp_state/temp_state_anon.py
+++ functions/temp_state/temp_state_anon.py
@@ -109,25 +109,20 @@ def next_anon1(update, id_telegram, stato):
                 InlineKeyboardButton(text="ANONIMO", callback_data="0")
             ],
             [
-                InlineKeyboardButton(text="IDENTITA' 1", callback_data="1")
+                InlineKeyboardButton(text="1", callback_data="1"),
+                InlineKeyboardButton(text="2", callback_data="2")
             ],
             [
-                InlineKeyboardButton(text="IDENTITA' 2", callback_data="2")
+                InlineKeyboardButton(text="3", callback_data="3"),
+                InlineKeyboardButton(text="4", callback_data="4")
             ],
             [
-                InlineKeyboardButton(text="IDENTITA' 3", callback_data="3")
+                InlineKeyboardButton(text="5", callback_data="5"),
+                InlineKeyboardButton(text="6", callback_data="6")
             ],
             [
-                InlineKeyboardButton(text="IDENTITA' 4", callback_data="4")
-            ],
-            [
-                InlineKeyboardButton(text="IDENTITA' 5", callback_data="5")
-            ],
-            [
-                InlineKeyboardButton(text="IDENTITA' 6", callback_data="6")
-            ],
-            [
-                InlineKeyboardButton(text="IDENTITA' 7", callback_data="7")
+                InlineKeyboardButton(text="7", callback_data="7"),
+                InlineKeyboardButton(text="8", callback_data="8")
             ]
 
         ]
