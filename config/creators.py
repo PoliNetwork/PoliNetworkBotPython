c91bfee1b13cfa4d4af3bcb5aded32dae2ebc3df
Federico Armellini <armellini.federico@gmail.com>
2020-07-05 21:19:52 +0200

    Update creators.py

--- config/creators.py
+++ config/creators.py
@@ -2,16 +2,18 @@ creators = ["policreator", "policreator2", "policreator3",
             "policreator4", "policreator5", "armef97", "poliadmin"]
 subcreators = ["carlogiova", "giovannieffe777", "testpolinetwork", "albus25", "deet98"]
 owners = [5651789]  # id of @ArmeF97
-ban_all = [5651789, # @ArmeF97
-           149620444 # Raif
+ban_all = [5651789,  # @ArmeF97
+           149620444  # Raif
            ]
 
 me = -1
-allowed_spam = [5651789  # @ArmeF97
+allowed_spam = [5651789,  # @ArmeF97
+                107050697,  # @EliaMaggioni
+                149620444  # @Raif9
                 ]
 allowed_no_username = [777000]  # telegram
 
 assoc_owners = [
-            5651789,  # @ArmeF97
-            203350312
-        ]
+    5651789,  # @ArmeF97
+    203350312
+]
