ba2063730ad6cb2fde74edba64352dae49aaaa5a
armellini.federico@gmail.com <armellini.federico@gmail.com>
2020-09-14 10:32:19 +0200

    Update creators.py

--- config/creators.py
+++ config/creators.py
@@ -2,7 +2,7 @@ creators = ["policreator", "policreator2", "policreator3",
             "policreator4", "policreator5", "armef97", "poliadmin"]
 subcreators = ["carlogiova", "giovannieffe777", "testpolinetwork",
                "albus25", "deet98", "alberto_fattori", "scala98",
-               "giulia_ye", "andre_crc"]
+               "giulia_ye", "andre_crc", "perularrabeiti"]
 owners = [5651789]  # id of @ArmeF97
 ban_all = [5651789,  # @ArmeF97
            149620444  # Raif
